e1963c0cd2e3a4f7d82beacc3b7c4adc820fba24
PatelDeep007 <46385810+PatelDeep007@users.noreply.github.com>
2019-09-01 20:39:57 +0530

    My first react app

---
+++ precache-manifest.679c2cefc5b7d75a6648a6bf540167d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7144588d95624e8110253f68564d0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "24eba1e1692531b7b30d",
+    "url": "/static/css/main.7c1c472c.chunk.css"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/static/js/2.52355532.chunk.js"
+  },
+  {
+    "revision": "24eba1e1692531b7b30d",
+    "url": "/static/js/main.0a778458.chunk.js"
+  },
+  {
+    "revision": "31175d55b7ad255ef221",
+    "url": "/static/js/runtime~main.6237261c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
